b932171715df94b52e2a276f7876e78ceaeb630c
Shigin D <empirefp@mail.ru>
2022-09-14 15:51:35 +0300

    add task, add freezed

---
+++ lib/entities/my_object_dto.freezed.dart
@@ -0,0 +1,380 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'my_object_dto.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+mixin _$MyObjectDto {
+  int get id => throw _privateConstructorUsedError;
+  String get name => throw _privateConstructorUsedError;
+  SwitchDto get switch1 => throw _privateConstructorUsedError;
+  SwitchDto get switch2 => throw _privateConstructorUsedError;
+  SwitchDto get switch3 => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $MyObjectDtoCopyWith<MyObjectDto> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $MyObjectDtoCopyWith<$Res> {
+  factory $MyObjectDtoCopyWith(
+          MyObjectDto value, $Res Function(MyObjectDto) then) =
+      _$MyObjectDtoCopyWithImpl<$Res>;
+  $Res call(
+      {int id,
+      String name,
+      SwitchDto switch1,
+      SwitchDto switch2,
+      SwitchDto switch3});
+
+  $SwitchDtoCopyWith<$Res> get switch1;
+  $SwitchDtoCopyWith<$Res> get switch2;
+  $SwitchDtoCopyWith<$Res> get switch3;
+}
+
+/// @nodoc
+class _$MyObjectDtoCopyWithImpl<$Res> implements $MyObjectDtoCopyWith<$Res> {
+  _$MyObjectDtoCopyWithImpl(this._value, this._then);
+
+  final MyObjectDto _value;
+  // ignore: unused_field
+  final $Res Function(MyObjectDto) _then;
+
+  @override
+  $Res call({
+    Object? id = freezed,
+    Object? name = freezed,
+    Object? switch1 = freezed,
+    Object? switch2 = freezed,
+    Object? switch3 = freezed,
+  }) {
+    return _then(_value.copyWith(
+      id: id == freezed
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int,
+      name: name == freezed
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      switch1: switch1 == freezed
+          ? _value.switch1
+          : switch1 // ignore: cast_nullable_to_non_nullable
+              as SwitchDto,
+      switch2: switch2 == freezed
+          ? _value.switch2
+          : switch2 // ignore: cast_nullable_to_non_nullable
+              as SwitchDto,
+      switch3: switch3 == freezed
+          ? _value.switch3
+          : switch3 // ignore: cast_nullable_to_non_nullable
+              as SwitchDto,
+    ));
+  }
+
+  @override
+  $SwitchDtoCopyWith<$Res> get switch1 {
+    return $SwitchDtoCopyWith<$Res>(_value.switch1, (value) {
+      return _then(_value.copyWith(switch1: value));
+    });
+  }
+
+  @override
+  $SwitchDtoCopyWith<$Res> get switch2 {
+    return $SwitchDtoCopyWith<$Res>(_value.switch2, (value) {
+      return _then(_value.copyWith(switch2: value));
+    });
+  }
+
+  @override
+  $SwitchDtoCopyWith<$Res> get switch3 {
+    return $SwitchDtoCopyWith<$Res>(_value.switch3, (value) {
+      return _then(_value.copyWith(switch3: value));
+    });
+  }
+}
+
+/// @nodoc
+abstract class _$$_MyObjectDtoCopyWith<$Res>
+    implements $MyObjectDtoCopyWith<$Res> {
+  factory _$$_MyObjectDtoCopyWith(
+          _$_MyObjectDto value, $Res Function(_$_MyObjectDto) then) =
+      __$$_MyObjectDtoCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {int id,
+      String name,
+      SwitchDto switch1,
+      SwitchDto switch2,
+      SwitchDto switch3});
+
+  @override
+  $SwitchDtoCopyWith<$Res> get switch1;
+  @override
+  $SwitchDtoCopyWith<$Res> get switch2;
+  @override
+  $SwitchDtoCopyWith<$Res> get switch3;
+}
+
+/// @nodoc
+class __$$_MyObjectDtoCopyWithImpl<$Res> extends _$MyObjectDtoCopyWithImpl<$Res>
+    implements _$$_MyObjectDtoCopyWith<$Res> {
+  __$$_MyObjectDtoCopyWithImpl(
+      _$_MyObjectDto _value, $Res Function(_$_MyObjectDto) _then)
+      : super(_value, (v) => _then(v as _$_MyObjectDto));
+
+  @override
+  _$_MyObjectDto get _value => super._value as _$_MyObjectDto;
+
+  @override
+  $Res call({
+    Object? id = freezed,
+    Object? name = freezed,
+    Object? switch1 = freezed,
+    Object? switch2 = freezed,
+    Object? switch3 = freezed,
+  }) {
+    return _then(_$_MyObjectDto(
+      id: id == freezed
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int,
+      name: name == freezed
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      switch1: switch1 == freezed
+          ? _value.switch1
+          : switch1 // ignore: cast_nullable_to_non_nullable
+              as SwitchDto,
+      switch2: switch2 == freezed
+          ? _value.switch2
+          : switch2 // ignore: cast_nullable_to_non_nullable
+              as SwitchDto,
+      switch3: switch3 == freezed
+          ? _value.switch3
+          : switch3 // ignore: cast_nullable_to_non_nullable
+              as SwitchDto,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_MyObjectDto implements _MyObjectDto {
+  const _$_MyObjectDto(
+      {required this.id,
+      required this.name,
+      required this.switch1,
+      required this.switch2,
+      required this.switch3});
+
+  @override
+  final int id;
+  @override
+  final String name;
+  @override
+  final SwitchDto switch1;
+  @override
+  final SwitchDto switch2;
+  @override
+  final SwitchDto switch3;
+
+  @override
+  String toString() {
+    return 'MyObjectDto(id: $id, name: $name, switch1: $switch1, switch2: $switch2, switch3: $switch3)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_MyObjectDto &&
+            const DeepCollectionEquality().equals(other.id, id) &&
+            const DeepCollectionEquality().equals(other.name, name) &&
+            const DeepCollectionEquality().equals(other.switch1, switch1) &&
+            const DeepCollectionEquality().equals(other.switch2, switch2) &&
+            const DeepCollectionEquality().equals(other.switch3, switch3));
+  }
+
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      const DeepCollectionEquality().hash(id),
+      const DeepCollectionEquality().hash(name),
+      const DeepCollectionEquality().hash(switch1),
+      const DeepCollectionEquality().hash(switch2),
+      const DeepCollectionEquality().hash(switch3));
+
+  @JsonKey(ignore: true)
+  @override
+  _$$_MyObjectDtoCopyWith<_$_MyObjectDto> get copyWith =>
+      __$$_MyObjectDtoCopyWithImpl<_$_MyObjectDto>(this, _$identity);
+}
+
+abstract class _MyObjectDto implements MyObjectDto {
+  const factory _MyObjectDto(
+      {required final int id,
+      required final String name,
+      required final SwitchDto switch1,
+      required final SwitchDto switch2,
+      required final SwitchDto switch3}) = _$_MyObjectDto;
+
+  @override
+  int get id;
+  @override
+  String get name;
+  @override
+  SwitchDto get switch1;
+  @override
+  SwitchDto get switch2;
+  @override
+  SwitchDto get switch3;
+  @override
+  @JsonKey(ignore: true)
+  _$$_MyObjectDtoCopyWith<_$_MyObjectDto> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+mixin _$SwitchDto {
+  SwitchType get type => throw _privateConstructorUsedError;
+  bool get value => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $SwitchDtoCopyWith<SwitchDto> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $SwitchDtoCopyWith<$Res> {
+  factory $SwitchDtoCopyWith(SwitchDto value, $Res Function(SwitchDto) then) =
+      _$SwitchDtoCopyWithImpl<$Res>;
+  $Res call({SwitchType type, bool value});
+}
+
+/// @nodoc
+class _$SwitchDtoCopyWithImpl<$Res> implements $SwitchDtoCopyWith<$Res> {
+  _$SwitchDtoCopyWithImpl(this._value, this._then);
+
+  final SwitchDto _value;
+  // ignore: unused_field
+  final $Res Function(SwitchDto) _then;
+
+  @override
+  $Res call({
+    Object? type = freezed,
+    Object? value = freezed,
+  }) {
+    return _then(_value.copyWith(
+      type: type == freezed
+          ? _value.type
+          : type // ignore: cast_nullable_to_non_nullable
+              as SwitchType,
+      value: value == freezed
+          ? _value.value
+          : value // ignore: cast_nullable_to_non_nullable
+              as bool,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$$_SwitchDtoCopyWith<$Res> implements $SwitchDtoCopyWith<$Res> {
+  factory _$$_SwitchDtoCopyWith(
+          _$_SwitchDto value, $Res Function(_$_SwitchDto) then) =
+      __$$_SwitchDtoCopyWithImpl<$Res>;
+  @override
+  $Res call({SwitchType type, bool value});
+}
+
+/// @nodoc
+class __$$_SwitchDtoCopyWithImpl<$Res> extends _$SwitchDtoCopyWithImpl<$Res>
+    implements _$$_SwitchDtoCopyWith<$Res> {
+  __$$_SwitchDtoCopyWithImpl(
+      _$_SwitchDto _value, $Res Function(_$_SwitchDto) _then)
+      : super(_value, (v) => _then(v as _$_SwitchDto));
+
+  @override
+  _$_SwitchDto get _value => super._value as _$_SwitchDto;
+
+  @override
+  $Res call({
+    Object? type = freezed,
+    Object? value = freezed,
+  }) {
+    return _then(_$_SwitchDto(
+      type: type == freezed
+          ? _value.type
+          : type // ignore: cast_nullable_to_non_nullable
+              as SwitchType,
+      value: value == freezed
+          ? _value.value
+          : value // ignore: cast_nullable_to_non_nullable
+              as bool,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_SwitchDto implements _SwitchDto {
+  const _$_SwitchDto({required this.type, required this.value});
+
+  @override
+  final SwitchType type;
+  @override
+  final bool value;
+
+  @override
+  String toString() {
+    return 'SwitchDto(type: $type, value: $value)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_SwitchDto &&
+            const DeepCollectionEquality().equals(other.type, type) &&
+            const DeepCollectionEquality().equals(other.value, value));
+  }
+
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      const DeepCollectionEquality().hash(type),
+      const DeepCollectionEquality().hash(value));
+
+  @JsonKey(ignore: true)
+  @override
+  _$$_SwitchDtoCopyWith<_$_SwitchDto> get copyWith =>
+      __$$_SwitchDtoCopyWithImpl<_$_SwitchDto>(this, _$identity);
+}
+
+abstract class _SwitchDto implements SwitchDto {
+  const factory _SwitchDto(
+      {required final SwitchType type,
+      required final bool value}) = _$_SwitchDto;
+
+  @override
+  SwitchType get type;
+  @override
+  bool get value;
+  @override
+  @JsonKey(ignore: true)
+  _$$_SwitchDtoCopyWith<_$_SwitchDto> get copyWith =>
+      throw _privateConstructorUsedError;
+}
